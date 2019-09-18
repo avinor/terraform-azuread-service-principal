1096898c0ba26ac6190fcb189c434516d46a57ac
Terje Torkelsen <terje.torkelsen@avinor.no>
2019-09-18 14:19:03 +0200

    Added object_id to output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "object_id" {
+  description = "The object id of service principal. Can be used to assign roles to user."
+  value       = azuread_service_principal.sp.object_id
+}
+
 output "client_id" {
   description = "The application id of AzureAD application created."
   value       = azuread_application.sp.application_id
