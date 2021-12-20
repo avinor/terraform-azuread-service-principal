7b25ebb8d00f97f11dc758b95dbc36fad4496dc7
Yngve Hansen <6208039+yngveh@users.noreply.github.com>
2021-12-20 11:35:09 +0100

    Pinning azuread provider version to 1.6.0 (#3)

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = ">= 1.4.0"
+      version = "~> 1.6.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
