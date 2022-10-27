51ba4e342b7393b82fe73c4efa7f82da406aa390
Prykhodko, Pavlo <pavlo.prykhodko@avinor.no>
2022-10-27 13:53:45 +0200

    Fix output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,9 @@ output "client_id" {
   description = "The application id of AzureAD application created."
   value       = azuread_application.sp.application_id
 }
+
+output "client_secret" {
+  description = "Password for service principal."
+  value       = azuread_service_principal_password.sp.value
+  sensitive   = true
+}
