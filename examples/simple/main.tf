1147efc5eb8f208c4b75ac59a5f614494355cda0
Yngve Hansen <yngve.hansen2@avinor.no>
2022-10-28 10:01:07 +0200

    Terraform fmt for examples

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,6 +1,6 @@
 module "service_principal" {
-    source = "../../"
+  source = "../../"
 
-    name = "simple-sp"
-    end_date = "2020-01-01T00:00:00Z"
+  name     = "simple-sp"
+  end_date = "2020-01-01T00:00:00Z"
 }
